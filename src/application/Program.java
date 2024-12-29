595d344955adf1c198c9e353ae54c9645875fb0e
Jairo Alberto <jairo.albertto@gmail.com>
2024-12-29 18:20:45 -0300

    Lambda com Comparator

---
+++ src/application/Program.java
@@ -0,0 +1,23 @@
+package application;
+
+import entities.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Program {
+    public static void main(String[] args) {
+
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product(900.00, "TV"));
+        list.add(new Product(1200.00, "Notebook"));
+        list.add(new Product(450.00, "Tablet"));
+
+        list.sort((p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
+
+        for (Product p : list) {
+            System.out.println(p);
+        }
+    }
+}
